96621e5070e2f158592e25ace2409d4eb9a121cf
kobe4cn <kevin.yang.xgz@gmail.com>
2023-08-02 17:43:20 +0800

    指令精调

--- scripts/training/run_sft.sh
+++ scripts/training/run_sft.sh
@@ -1,3 +1,4 @@
+########参数部分########
 lr=1e-4
 lora_rank=64
 lora_alpha=128
@@ -5,18 +6,20 @@ lora_trainable="q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj"
 modules_to_save="embed_tokens,lm_head"
 lora_dropout=0.05
 
-pretrained_model=path/to/hf/llama-2/or/merged/llama-2/dir/or/model_id
-chinese_tokenizer_path=path/to/chinese/llama-2/tokenizer/dir
-dataset_dir=path/to/sft/data/dir
+pretrained_model=/mnt/mydisk/llama2_models/Llama-2-7b-hf
+chinese_tokenizer_path=/mnt/mydisk/llama2-merage-output/merge-llama2-chinese-alpaca-7B
+dataset_dir=/mnt/mydisk/LLaMA2-Alpaca2/data/train
 per_device_train_batch_size=1
 per_device_eval_batch_size=1
-gradient_accumulation_steps=8
-output_dir=output_dir
-peft_model=path/to/peft/model/dir
-validation_file=validation_file_name
+gradient_accumulation_steps=1
+output_dir=/mnt/mydisk/llama2_sft_output
+peft_model=/mnt/mydisk/llama2_models/chinese-alpaca-2-lora-7b
+validation_file=/mnt/mydisk/LLaMA2-Alpaca2/data/eval/alpaca_data_zh_51k.json
+max_seq_length=1024
 
 deepspeed_config_file=ds_zero2_no_offload.json
 
+########启动命令########
 torchrun --nnodes 1 --nproc_per_node 1 run_clm_sft_with_peft.py \
     --deepspeed ${deepspeed_config_file} \
     --model_name_or_path ${pretrained_model} \
@@ -29,7 +32,7 @@ torchrun --nnodes 1 --nproc_per_node 1 run_clm_sft_with_peft.py \
     --do_eval \
     --seed $RANDOM \
     --fp16 \
-    --num_train_epochs 1 \
+    --num_train_epochs 2 \
     --lr_scheduler_type cosine \
     --learning_rate ${lr} \
     --warmup_ratio 0.03 \
@@ -39,20 +42,15 @@ torchrun --nnodes 1 --nproc_per_node 1 run_clm_sft_with_peft.py \
     --save_strategy steps \
     --save_total_limit 3 \
     --evaluation_strategy steps \
-    --eval_steps 100 \
-    --save_steps 200 \
+    --eval_steps 250 \
+    --save_steps 500 \
     --gradient_accumulation_steps ${gradient_accumulation_steps} \
     --preprocessing_num_workers 8 \
-    --max_seq_length 1024 \
+    --max_seq_length ${max_seq_length} \
     --output_dir ${output_dir} \
     --overwrite_output_dir \
     --ddp_timeout 30000 \
     --logging_first_step True \
-    --lora_rank ${lora_rank} \
-    --lora_alpha ${lora_alpha} \
-    --trainable ${lora_trainable} \
-    --modules_to_save ${modules_to_save} \
-    --lora_dropout ${lora_dropout} \
     --torch_dtype float16 \
     --validation_file ${validation_file} \
     --peft_path ${peft_model} \
